ab33084cb931ebcb5221db51494d4cb448e40d0e
Eddy <moura@ciandt.com>
2022-11-18 15:34:17 -0300

    Configura Estados Coming Soon

--- app/src/main/java/com/example/appthemoviedb/presenter/moviescomingsoon/ComingSoonMoviesFragment.kt
+++ app/src/main/java/com/example/appthemoviedb/presenter/moviescomingsoon/ComingSoonMoviesFragment.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.paging.LoadState
 import com.example.appthemoviedb.databinding.FragmentComingSoonMoviesBinding
+import com.example.appthemoviedb.presenter.moviesloadmore.MoviesLoadStateAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -37,6 +41,7 @@ class ComingSoonMoviesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initComingSoonMoviesAdapter()
+        observeInitialLoadState()
 
         lifecycleScope.launch {
             viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
@@ -52,8 +57,50 @@ class ComingSoonMoviesFragment : Fragment() {
         with(binding.recyclerMoviesComingSoon) {
             scrollToPosition(0)
             setHasFixedSize(true)
-            adapter = moviesComingSoonMoviesAdapter
+            adapter = moviesComingSoonMoviesAdapter.withLoadStateFooter(
+                footer = MoviesLoadStateAdapter { moviesComingSoonMoviesAdapter.retry() }
+            )
         }
+    }
+
+    private fun observeInitialLoadState() {
+        lifecycleScope.launch {
+            moviesComingSoonMoviesAdapter.loadStateFlow.collectLatest { loadState ->
+                binding.flipperMoviesComingSoon.displayedChild =
+                    when (loadState.refresh) {
+                        is LoadState.Loading -> {
+                            setShimmerVisibility(true)
+                            FLIPPER_CHILD_LOADING
+                        }
+                        is LoadState.NotLoading -> {
+                            setShimmerVisibility(false)
+                            FLIPPER_CHILD_SUCCESS
+                        }
+                        is LoadState.Error -> {
+                            setShimmerVisibility(false)
+                            binding.includeViewMoviesErrorState.buttonRetry.setOnClickListener {
+                                moviesComingSoonMoviesAdapter.refresh()
+                            }
+                            FLIPPER_CHILD_ERROR
+                        }
+                    }
+            }
+        }
+    }
+
+    private fun setShimmerVisibility(visibility: Boolean) {
+        binding.includeViewMoviesLoadingState.shimmerMovies.run {
+            isVisible = visibility
+            if (visibility) {
+                startShimmer()
+            } else stopShimmer()
+        }
+    }
+
+    companion object {
+        private const val FLIPPER_CHILD_LOADING = 0
+        private const val FLIPPER_CHILD_SUCCESS = 1
+        private const val FLIPPER_CHILD_ERROR = 2
     }
 
 }
